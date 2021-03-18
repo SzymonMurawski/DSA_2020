fa4a5af8b337d2fe70e8a253ea1ef7f11ecae33a
Szymon Murawski <szymon.murawski@gmail.com>
2021-03-18 12:40:35 +0100

    Introducing GoLGrid class

---
+++ GameOfLife/GameOfLife/GoLGrid.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameOfLife
+{
+    class GoLGrid
+    {
+        private bool[,] Grid;
+        public GoLGrid()
+        {
+            Grid = new bool[20, 20];
+        }
+
+        public void Initialize()
+        {
+            Grid[1, 1] = true;
+            Grid[1, 2] = true;
+            Grid[1, 3] = true;
+            Grid[2, 3] = true;
+            Grid[2, 5] = true;
+            Grid[3, 5] = true;
+            Grid[4, 5] = true;
+        }
+        public void Display()
+        {
+            Console.Clear();
+            for (int y = 0; y < Grid.GetLength(0); y++)
+            {
+                for (int x = 0; x < Grid.GetLength(1); x++)
+                {
+                    if (Grid[x, y])
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void Transform()
+        {
+            bool[,] new_grid = new bool[Grid.GetLength(0), Grid.GetLength(1)];
+            for (int y = 0; y < Grid.GetLength(0); y++)
+            {
+                for (int x = 0; x < Grid.GetLength(1); x++)
+                {
+                    int aliveNeighbours = NumberOfAliveNeighbours(x, y);
+                    if (Grid[x, y]) // cell is alive
+                    {
+                        if (aliveNeighbours == 2 || aliveNeighbours == 3)
+                        {
+                            new_grid[x, y] = true;
+                        }
+                        else
+                        {
+                            new_grid[x, y] = false;
+                        }
+
+                    }
+                    else // cell is dead
+                    {
+                        if (aliveNeighbours == 3)
+                        {
+                            new_grid[x, y] = true;
+                        }
+                        else
+                        {
+                            new_grid[x, y] = false;
+                        }
+                    }
+                }
+            }
+            Grid = new_grid;
+        }
+
+        public int NumberOfAliveNeighbours(int cell_x, int cell_y)
+        {
+            int aliveNeighbours = 0;
+            int dx = Grid.GetLength(0);
+            int dy = Grid.GetLength(1);
+            // We are using periodic boundary conditions, hence the modulo division
+            if (Grid[(cell_x - 1 + dx) % dx, (cell_y - 1 + dy) % dy])
+            {
+                aliveNeighbours++;
+            }
+            if (Grid[cell_x, (cell_y - 1 + dy) % dy])
+            {
+                aliveNeighbours++;
+            }
+            if (Grid[(cell_x + 1) % dx, (cell_y - 1 + dy) % dy])
+            {
+                aliveNeighbours++;
+            }
+
+            if (Grid[(cell_x - 1 + dx) % dx, cell_y])
+            {
+                aliveNeighbours++;
+            }
+            if (Grid[(cell_x + 1) % dx, cell_y])
+            {
+                aliveNeighbours++;
+            }
+
+            if (Grid[(cell_x - 1 + dx) % dx, (cell_y + 1) % dy])
+            {
+                aliveNeighbours++;
+            }
+            if (Grid[cell_x, (cell_y + 1) % dy])
+            {
+                aliveNeighbours++;
+            }
+            if (Grid[(cell_x + 1) % dx, (cell_y + 1) % dy])
+            {
+                aliveNeighbours++;
+            }
+
+            return aliveNeighbours;
+        }
+    }
+}
--- GameOfLife/GameOfLife/Program.cs
+++ GameOfLife/GameOfLife/Program.cs
@@ -6,117 +6,14 @@ namespace GameOfLife
     {
         static void Main(string[] args)
         {
-            bool[,] grid = GridInitialization();
+            GoLGrid gameGrid = new GoLGrid();
+            gameGrid.Initialize();
             while (true)
             {
                 System.Threading.Thread.Sleep(1000);
-                GridDisplay(grid);
-                grid = GridTransformation(grid);
+                gameGrid.Display();
+                gameGrid.Transform();
             }
-        }
-
-        static bool[,] GridInitialization()
-        {
-            bool[,] grid = new bool[20, 20];
-            grid[1, 1] = true;
-            grid[1, 2] = true;
-            grid[1, 3] = true;
-            return grid;
-        }
-
-        static void GridDisplay(bool[,] grid)
-        {
-            Console.Clear();
-            for (int y = 0; y < grid.GetLength(0); y++)
-            {
-                for (int x = 0; x < grid.GetLength(1); x++)
-                {
-                    if (grid[x, y])
-                    {
-                        Console.Write("X");
-                    } else
-                    {
-                        Console.Write(".");
-                    }
-                }
-                Console.WriteLine();
-            }
-        }
-
-        static bool[,] GridTransformation(bool[,] grid)
-        {
-            bool[,] new_grid = new bool[grid.GetLength(0), grid.GetLength(1)];
-            for (int y = 0; y < grid.GetLength(0); y++)
-            {
-                for (int x = 0; x < grid.GetLength(1); x++)
-                {
-                    int aliveNeighbours = numberOfAliveNeighbours(x, y, grid);
-                    if (grid[x, y]) // cell is alive
-                    {
-                        if (aliveNeighbours == 2 || aliveNeighbours == 3)
-                        {
-                            new_grid[x, y] = true;
-                        } else
-                        {
-                            new_grid[x, y] = false;
-                        }
-
-                    } else // cell is dead
-                    {
-                        if (aliveNeighbours == 3)
-                        {
-                            new_grid[x, y] = true;
-                        } else
-                        {
-                            new_grid[x, y] = false;
-                        }
-                    }
-                }
-            }
-            return new_grid;
-        }
-
-        static int numberOfAliveNeighbours(int cell_x, int cell_y, bool[,] grid)
-        {
-            int aliveNeighbours = 0;
-            int dx = grid.GetLength(0);
-            int dy = grid.GetLength(1);
-            if(grid[(cell_x  - 1 + dx) % dx, (cell_y  - 1 + dy) % dy])
-            {
-                aliveNeighbours++;
-            }
-            if (grid[cell_x, (cell_y  - 1 + dy) % dy])
-            {
-                aliveNeighbours++;
-            }
-            if (grid[(cell_x + 1) % dx, (cell_y  - 1 + dy) % dy])
-            {
-                aliveNeighbours++;
-            }
-
-            if (grid[(cell_x  - 1 + dx) % dx, cell_y])
-            {
-                aliveNeighbours++;
-            }
-            if (grid[(cell_x + 1) % dx, cell_y])
-            {
-                aliveNeighbours++;
-            }
-
-            if (grid[(cell_x  - 1 + dx) % dx, (cell_y + 1) % dy])
-            {
-                aliveNeighbours++;
-            }
-            if (grid[cell_x, (cell_y + 1) % dy])
-            {
-                aliveNeighbours++;
-            }
-            if (grid[(cell_x + 1) % dx, (cell_y + 1) % dy])
-            {
-                aliveNeighbours++;
-            }
-
-            return aliveNeighbours;
         }
     }
 }
