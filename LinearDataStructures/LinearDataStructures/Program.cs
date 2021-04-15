fa952bea0abe929d6be163365b9b5a9ba7493d04
Szymon Murawski <szymon.murawski@comarch.pl>
2021-04-15 11:48:36 +0200

    LinearDataStructures init

---
+++ LinearDataStructures/LinearDataStructures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinearDataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
