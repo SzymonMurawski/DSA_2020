0bdc898496a51c3c1489b8c4bfd95b828cb675e8
Szymon Murawski <szymon.murawski@gmail.com>
2021-04-22 12:43:57 +0200

    Array based Stack implementation

--- LinearDataStructures/LinearDataStructures/Stack.cs
+++ LinearDataStructures/LinearDataStructures/Stack.cs
@@ -6,26 +6,39 @@ namespace LinearDataStructures
 {
     public class Stack
     {
+        private int[] InternalArray;
         public int Count;
         public Stack()
         {
             Count = 0;
+            InternalArray = new int[100];
         }
         // this should add a new element at the top of the stack
         public void Push(int value)
         {
-
+            InternalArray[Count] = value;
+            Count++;
         }
 
         // this method should remove the element from the top of the stack and return it
         public int Pop()
         {
-            throw new NotImplementedException();
+            if (Count == 0)
+            {
+                throw new NullReferenceException();
+            }
+            int returnValue = InternalArray[Count - 1];
+            Count--;
+            return returnValue;
         }
 
         public int Peek()
         {
-            throw new NotImplementedException();
+            if (Count == 0)
+            {
+                throw new NullReferenceException();
+            }
+            return InternalArray[Count - 1];
         }
     }
 }
